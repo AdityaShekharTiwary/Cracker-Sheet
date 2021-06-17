1e72994f8ba72e117b994dbc03fc220ec7689709
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-06-17 08:07:03 +0530

    Binary tree to bst

---
+++ binary_tree_to_bst.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  // The given root is the root of the Binary Tree
+  // Return the root of the generated BST
+  void convert(Node* root, vector<int> &v) {
+    if (!root) return ;
+    convert(root->left, v);
+    v.push_back(root->data);
+    convert(root->right, v);
+  }
+
+  void solve(Node* root, vector<int> &v, int &i) {
+    if (!root) return ;
+    solve(root->left, v, i);
+    root->data = v[i++];
+    solve(root->right, v, i);
+  }
+  Node *binaryTreeToBST (Node *root)
+  {
+    vector<int> v;
+    convert(root, v);
+    sort(v.begin(), v.end());
+    int i = 0;
+    solve(root, v, i);
+    return root;
+  }
+};
