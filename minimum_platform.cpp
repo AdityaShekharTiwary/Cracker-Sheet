dc73a217c3209100ceb372703159c66a49a12c99
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-06-18 19:56:24 +0530

    Minimum platform

---
+++ minimum_platform.cpp
@@ -0,0 +1,13 @@
+int findPlatform(int arr[], int dep[], int n)
+{
+   sort(arr, arr + n);
+   sort(dep, dep + n);
+   int ans = 1;
+   int j = 0;
+   for (int i = 1; i < n; i++) {
+      if (arr[i] <= dep[j]) ans++;
+      else j++;
+   }
+   return ans;
+
+}
